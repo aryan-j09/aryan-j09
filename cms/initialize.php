a4b555abf07f7f7d026eda13013714b8cd280197
aryan-j09 <aryan.n.jain45@gmail.com>
2026-03-17 09:59:14 +0530

    Remove hardcoded secrets

--- cms/initialize.php
+++ cms/initialize.php
@@ -1,10 +1,31 @@
 <?php
-$dev_data = array('id'=>'-1','firstname'=>'Aryan','lastname'=>'Jain','username'=>'Aryan_jain','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url',"https://sbpanchal.com/cms/");
+$dev_data = array(
+	'id' => '-1',
+	'firstname' => 'Aryan',
+	'lastname' => 'Jain',
+	'username' => 'Aryan_jain',
+	'password' => '',
+	'last_login' => '',
+	'date_updated' => '',
+	'date_added' => ''
+);
+
+function env_or_default($key, $default = '') {
+	$value = getenv($key);
+	return ($value !== false && $value !== '') ? $value : $default;
+}
+
+if(!defined('base_url')) define('base_url', env_or_default('CMS_BASE_URL', "https://sbpanchal.com/cms/"));
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
-if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"sbpanchaldev");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"SBpanchal@devak2");
-if(!defined('DB_NAME')) define('DB_NAME',"Db_SB_Panchal_2022");
+
+if(!defined('DB_SERVER')) define('DB_SERVER', env_or_default('CMS_DB_SERVER', "localhost"));
+if(!defined('DB_USERNAME')) define('DB_USERNAME', env_or_default('CMS_DB_USERNAME'));
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD', env_or_default('CMS_DB_PASSWORD'));
+if(!defined('DB_NAME')) define('DB_NAME', env_or_default('CMS_DB_NAME'));
+
+if(DB_USERNAME === '' || DB_PASSWORD === '' || DB_NAME === ''){
+	http_response_code(500);
+	exit('Server configuration error: missing database environment variables.');
+}
 ?>
